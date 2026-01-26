78e30ccfdb198ba8bca6a77da29004adb4f37c2f
backingheart <62789253+backingheart@users.noreply.github.com>
2026-01-26 16:51:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# 1. 修改默认IP为 192.168.5.1
+# 1. 修改默认IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-# 2. 修改分区大小：内核256MB，根分区1024MB
-# 注意：这一步会寻找 .config 里的默认值并替换
-sed -i 's/CONFIG_TARGET_KERNEL_PARTSIZE=16/CONFIG_TARGET_KERNEL_PARTSIZE=256/g' .config
-sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=160/CONFIG_TARGET_ROOTFS_PARTSIZE=1024/g' .config
+# 2. 强力锁定分区大小（无论默认值是多少，全部强制重写）
+sed -i '/CONFIG_TARGET_KERNEL_PARTSIZE/d' .config
+echo "CONFIG_TARGET_KERNEL_PARTSIZE=256" >> .config
+
+sed -i '/CONFIG_TARGET_ROOTFS_PARTSIZE/d' .config
+echo "CONFIG_TARGET_ROOTFS_PARTSIZE=1024" >> .config
