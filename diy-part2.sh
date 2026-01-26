a76b021e306c1ada49f921bf62d74dc678f0d171
backingheart <62789253+backingheart@users.noreply.github.com>
2026-01-26 16:58:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# 1. 修改默认IP
+
+## 1. 修改默认管理 IP 为 192.168.5.1
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-# 2. 强力锁定分区大小（无论默认值是多少，全部强制重写）
+## 2. 强力锁定分区大小，避免被源码默认值覆盖
+## 删除现有的分区设置，然后在文件末尾重新注入 256MB 内核和 1024MB 根分区
 sed -i '/CONFIG_TARGET_KERNEL_PARTSIZE/d' .config
 echo "CONFIG_TARGET_KERNEL_PARTSIZE=256" >> .config
 
 sed -i '/CONFIG_TARGET_ROOTFS_PARTSIZE/d' .config
 echo "CONFIG_TARGET_ROOTFS_PARTSIZE=1024" >> .config
+
+## 3. 设置默认主机名为 MyOpenwrt
+sed -i 's/OpenWrt/MyOpenwrt/g' package/base-files/files/bin/config_generate
