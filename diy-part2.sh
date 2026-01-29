7cac44c4e01aa8347d5a1a2d26bf1dfaf14e529a
backingheart <62789253+backingheart@users.noreply.github.com>
2026-01-30 03:25:01 +0800

    Update DIY script with IP and hostname changes

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-# Description: OpenWrt DIY script part 2 (Fixes & Configs)
-
-# 1. 修改默认 IP -> 192.168.5.1
+# 1. 改 IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-
-# 2. 修改主机名 -> N100-Commander
+# 2. 改名
 sed -i 's/ImmortalWrt/N100-Commander/g' package/base-files/files/bin/config_generate
-
-# -------------------------------------------------------------------------
-# 🚨 核心修复: apk mkndx 索引报错 (V3 必须保留)
-# -------------------------------------------------------------------------
-echo "Executing V3 Fix: Neutralizing apk mkndx..."
-find . -name "*.mk" -exec sed -i 's/apk mkndx/true/g' {} +
-
-# 双重保险
-if [ -f "include/image.mk" ]; then
-    sed -i 's/apk mkndx/true/g' include/image.mk
-fi
-# -------------------------------------------------------------------------
+# 3. 强制驱动 (保险起见)
+sed -i 's/CONFIG_PACKAGE_kmod-igc=m/CONFIG_PACKAGE_kmod-igc=y/g' .config || true
